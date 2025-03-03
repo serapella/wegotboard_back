cae19297afbb6cdafcf53be6611dca4ed2f3edd0
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-03-03 19:14:26 +0100

    fixed css so that it matches with Anoucks css mockup

--- src/components/ShopNowSection.tsx
+++ src/components/ShopNowSection.tsx
@@ -6,17 +6,17 @@ const ShopNowSection = () => {
       <div className={styles.shopNowCards}>
         <div>
           <img src="../mh_bg.png" alt="monster hunter boargame" />
-          <p>Hunt Monsters together with your party at home!</p>
+          <h3>Hunt Monsters together with your party at home!</h3>
           <button>Shop Now</button>
         </div>
         <div>
           <img src="../deadcells_bg.png" alt="deadcells boargame" />
-          <p>Fight your way through endless dungeons!</p>
+          <h3>Fight your way through endless dungeons!</h3>
           <button>Shop Now</button>
         </div>
         <div>
           <img src="../terraria_bg.png" alt="terraria boargame" />
-          <p>Terraria releases as a standalone boardgame!</p>
+          <h3>Terraria releases as a standalone boardgame!</h3>
           <button>Shop Now</button>
         </div>
       </div>
