5f25d26e2e252909cdd694f82e08c8fe504e24f7
DQuinn97 <quinten2407@gmail.com>
2025-03-03 10:56:18 +0100

    user model

---
+++ src/models/UserModel.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      first: { type: String, required: true, trim: true },
+      last: { type: String, required: true, trim: true },
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isSubscribed: {
+      type: Boolean,
+      default: false,
+    },
+    phoneNumber: {
+      type: String,
+      nullable: true,
+      trim: true,
+    },
+    location: {
+      address: {
+        type: String,
+        nullable: true,
+        trim: true,
+      },
+      city: {
+        type: String,
+        nullable: true,
+        trim: true,
+      },
+      postCode: {
+        type: String,
+        nullable: true,
+        trim: true,
+      },
+      country: {
+        type: String,
+        nullable: true,
+        trim: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Todo = mongoose.model("User", UserSchema);
