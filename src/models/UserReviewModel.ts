233eba77970cb04afecd045b7ee2939df4d44b5f
DQuinn97 <quinten2407@gmail.com>
2025-03-03 12:49:30 +0100

    user reviews

---
+++ src/models/UserReviewModel.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserReviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  review: {
+    type: String,
+    nullable: true,
+    trim: true,
+  },
+});
+
+export const Todo = mongoose.model("UserReview", UserReviewSchema);
