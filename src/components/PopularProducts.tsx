e747f6f8dc582c9141dc8a71720cf2c06a0a3352
Anouck Galoppin <anouck.galop@hotmail.com>
2025-03-06 11:19:23 +0100

    add category names

--- src/components/PopularProducts.tsx
+++ src/components/PopularProducts.tsx
@@ -8,12 +8,12 @@ const PopularProducts = () => {
         <h2>Popular products</h2>
         <ul>
           <li>All</li>
-          <li>Milk & Dairies</li>
-          <li>Cofees & Teas</li>
-          <li>Pet Foods</li>
-          <li>Meats</li>
-          <li>Vegetables</li>
-          <li>Fruits</li>
+          <li>Boardgames</li>
+          <li>Cardgames</li>
+          <li>Dice games</li>
+          <li>Party games</li>
+          <li>Travel games</li>
+          <li>Theme games</li>
         </ul>
       </div>
       <ProductGrid />
