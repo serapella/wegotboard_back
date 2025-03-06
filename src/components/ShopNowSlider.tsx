df0c2e16362d922144e282d7b1f9496cf6a7b5ad
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-03-06 14:02:10 +0100

    slideshow works, only buttons underneath

--- src/components/ShopNowSlider.tsx
+++ src/components/ShopNowSlider.tsx
@@ -6,37 +6,6 @@ const slideImages = ["../slide1.jpeg", "../slide2.jpg"];
 
 const ShopNowSlider = () => {
   return (
-    // <section className={styles.slider}>
-    //   <div className={styles.slide1}>
-    //     <div className={styles.slide1_content}>
-    //       <h3>
-    //         <span>100%</span> Fantastic Memories
-    //       </h3>
-    //       <h1>The best way to spend your time.</h1>
-    //       <p>
-    //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-    //       </p>
-    //       <button>Shop now</button>
-    //     </div>
-    //   </div>
-    //   <div className={styles.slide2}>
-    //     <div className={styles.slide2_content}>
-    //       <h3>
-    //         <span>100%</span> Fantastic Memories
-    //       </h3>
-    //       <h1>The best way to spend your time.</h1>
-    //       <p>
-    //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-    //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-    //       </p>
-    //       <div>
-    //         <input type="email" placeholder="Your email address" />
-    //         <button>Subscribe</button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
-
     <section className={styles.slideContainer}>
       <Fade>
         <div className={styles.eachFade}>
@@ -46,7 +15,42 @@ const ShopNowSlider = () => {
           <img src={slideImages[1]} alt="Slide 2" />
         </div>
       </Fade>
+      <div className={styles.radioButtons}>
+        <input type="radio" htmlFor="shopNowSlide" id="slide1" />
+        <input type="radio" htmlFor="shopNowSlide" id="slide2" />
+      </div>
     </section>
   );
 };
 export default ShopNowSlider;
+
+// <section className={styles.slider}>
+//   <div className={styles.slide1}>
+//     <div className={styles.slide1_content}>
+//       <h3>
+//         <span>100%</span> Fantastic Memories
+//       </h3>
+//       <h1>The best way to spend your time.</h1>
+//       <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+//       </p>
+//       <button>Shop now</button>
+//     </div>
+//   </div>
+//   <div className={styles.slide2}>
+//     <div className={styles.slide2_content}>
+//       <h3>
+//         <span>100%</span> Fantastic Memories
+//       </h3>
+//       <h1>The best way to spend your time.</h1>
+//       <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+//       </p>
+//       <div>
+//         <input type="email" placeholder="Your email address" />
+//         <button>Subscribe</button>
+//       </div>
+//     </div>
+//   </div>
+// </section>
