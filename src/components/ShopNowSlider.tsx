2c72b259e81a0778483c83c955585dff272d72e7
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-03-07 16:01:58 +0100

    removed unnecessary code

--- src/components/ShopNowSlider.tsx
+++ src/components/ShopNowSlider.tsx
@@ -1,13 +1,10 @@
 import styles from "../css_module/shopNowSlider.module.css";
 import { Fade } from "react-slideshow-image";
-import { useState } from "react";
 import "react-slideshow-image/dist/styles.css";
 
 const slideImages = ["../slide2.jpg", "../slide1.jpg"];
 
 const ShopNowSlider = () => {
-  const [activeSlide, setActiveSlide] = useState<number>(0);
-
   return (
     <section className={styles.slideContainer}>
       <div>
@@ -57,34 +54,3 @@ const ShopNowSlider = () => {
   );
 };
 export default ShopNowSlider;
-
-// <section className={styles.slider}>
-//   <div className={styles.slide1}>
-//     <div className={styles.slide1_content}>
-//       <h3>
-//         <span>100%</span> Fantastic Memories
-//       </h3>
-//       <h1>The best way to spend your time.</h1>
-//       <p>
-//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-//       </p>
-//
-//     </div>
-//   </div>
-//   <div className={styles.slide2}>
-//     <div className={styles.slide2_content}>
-//       <h3>
-//         <span>100%</span> Fantastic Memories
-//       </h3>
-//       <h1>The best way to spend your time.</h1>
-//       <p>
-//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-//       </p>
-//       <div>
-//         <input type="email" placeholder="Your email address" />
-//         <button>Subscribe</button>
-//       </div>
-//     </div>
-//   </div>
-// </section>
