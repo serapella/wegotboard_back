0e1d48297fcdf23c275fa39904748704acd51429
DQuinn97 <quinten2407@gmail.com>
2025-03-16 13:51:26 +0100

    +player min/max - price min + duration model + filter

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,8 +2,12 @@ export type ProductFilter = {
   language?: string;
   category?: string | object;
   price?: object;
-  playerCount?: object;
+  playerCount?: {
+    min?: object;
+    max?: object;
+  };
   difficulty?: ["easy", "medium", "hard"];
+  duration?: ["short", "medium", "long"];
   ageRating?: object;
   tags?: object;
 };
