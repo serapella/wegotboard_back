24896bf7ff23d5d7dd99c88472698e1050211c66
DQuinn97 <quinten2407@gmail.com>
2025-03-17 20:41:53 +0100

    fix playerCount filter

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,10 +2,8 @@ export type ProductFilter = {
   language?: string;
   category?: string | object;
   price?: object;
-  playerCount?: {
-    min?: object;
-    max?: object;
-  };
+  "playerCount.min": object;
+  "playerCount.max": object;
   difficulty?: ["easy", "medium", "hard"];
   duration?: ["short", "medium", "long"];
   ageRating?: object;
