d428dae1ac49d4eeba7fa2de3cf8d98a2387dbc7
Serap Cakmak <serap.cakmak@hotmail.com>
2025-03-19 12:52:47 +0100

    adapt related products section

--- src/pages/productDetailPage.tsx
+++ src/pages/productDetailPage.tsx
@@ -2,15 +2,15 @@ import PurchaseOptions from "../components/PurchaseOptions";
 import ProductSidebar from "../components/ProductSidebar";
 import ProductTabs from "../components/ProductTabs";
 import PopularProducts from "../components/PopularProducts";
-import styles from "../css_modules/productDetailPage.module.css";
+import styles from "../css_modules/ProductDetailpage.module.css";
 
 const ProductDetail = () => {
   return (
     <div className={styles.productDetailPage}>
       <section className={styles.productContent}>
         <div className={styles.mainContent}>
-          <PurchaseOptions />
           <ProductSidebar />
+          <PurchaseOptions />
         </div>
         <ProductTabs />
       </section>
