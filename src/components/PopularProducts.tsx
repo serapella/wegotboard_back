defcf065c5a800ffdadd1537ce0d85c64f99a179
DQuinn97 <quinten2407@gmail.com>
2025-03-20 15:13:14 +0100

    product filter limit

--- src/components/PopularProducts.tsx
+++ src/components/PopularProducts.tsx
@@ -8,12 +8,9 @@ const PopularProducts = () => {
         <h2>Popular products</h2>
         <ul>
           <li>All</li>
-          <li>Boardgames</li>
-          <li>Cardgames</li>
+          <li>Board games</li>
+          <li>Card games</li>
           <li>Dice games</li>
-          <li>Party games</li>
-          <li>Travel games</li>
-          <li>Theme games</li>
         </ul>
       </div>
       <ProductGrid />
