70b1c4b4685def6753502ea45cce59c5668c9232
Serap Cakmak <serap.cakmak@hotmail.com>
2025-03-22 15:38:41 +0100

    remove add + button and change the order of the content for the detail cariant

--- src/components/ProductCardInfo.tsx
+++ src/components/ProductCardInfo.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../css_modules/productCardInfo.module.css";
 import { BsCart3, BsStar, BsStarFill, BsStarHalf } from "react-icons/bs";
 import { ProductCardProps } from "../types";
@@ -8,18 +7,20 @@ const ProductCardInfo: React.FC<ProductCardProps> = ({
 }) => {
   return (
     <div className={styles.popular_item_text}>
-      <h5>Deep Rock Galactic Board Game</h5>
-      <i className={styles.ratingIcon}>
-        <BsStarFill />
-        <BsStarFill />
-        <BsStarFill />
-        <BsStarHalf />
-        <BsStar />
-      </i>
-      <p>(4.0)</p>
-      <p>
+      <p className={styles.vendor}>
         By <span>De Spelfanaat</span>
       </p>
+      <div className={styles.rating_container}>
+        <div className={styles.ratingIcon}>
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarFill />
+          <BsStarHalf />
+          <BsStar />
+        </div>
+        <div className={styles.rating_value}>(4.0)</div>
+      </div>
+      <h5>Deep Rock Galactic Board Game</h5>
       <div className={styles.popular_item_bottom}>
         <div className={styles.price_container}>
           <span className={styles.current_price}>$32.85</span>
