aa49fa356025dadc273984e35f7cff36b49ff167
DQuinn97 <quinten2407@gmail.com>
2025-03-22 21:08:23 +0100

    type not merged

--- src/types.ts
+++ src/types.ts
@@ -57,3 +57,26 @@ export interface ProductCardProps {
   variant?: "landing" | "detail";
   product?: Product;
 }
+
+export interface NewsFeed {
+  items: NewsFeedItem[];
+  image: string;
+  title: string;
+  description: string;
+  pubDate: string;
+  webMaster: string;
+  link: string;
+  language: string;
+  lastBuildDate: string;
+  backdrop: string;
+}
+
+export interface NewsFeedItem {
+  creator: string;
+  title: string;
+  link: string;
+  pubDate: string;
+  "dc:creator": string;
+  guid: string;
+  isoDate: Date;
+}
