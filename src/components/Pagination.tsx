eb9b3c4d47bc746b81ca22ad7fda245577457156
Anouck Galoppin <anouck.galop@hotmail.com>
2025-03-22 15:16:45 +0100

    add pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,15 +1,54 @@
 import styles from "../css_modules/pagination.module.css";
+import React from "react";
+// import { useEffect } from "react";
 
-const Pagination = () => {
-  return (
-    <div className={styles.gridPagination}>
-      <button>Previous</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>Next</button>
-    </div>
-  );
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  const handlePreviousPage = () => {
+    if (currentPage > 1) onPageChange(currentPage - 1);
+    const handleNextPage = () => {
+      if (currentPage < totalPages) onPageChange(currentPage + 1);
+    };
+
+    let pageNumbers = [currentPage];
+    if (currentPage === 1) {
+      pageNumbers = [1, 2, 3].filter((page) => page <= totalPages);
+    } else if (currentPage === totalPages) {
+      pageNumbers = [totalPages - 2, totalPages - 1, totalPages].filter(
+        (page) => page >= 1
+      );
+    } else {
+      pageNumbers = [currentPage - 1, currentPage, currentPage + 1].filter(
+        (page) => page <= totalPages
+      );
+    }
+
+    return (
+      <div className={styles.gridPagination}>
+        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+
+        {pageNumbers.map((page) => (
+          <button key={page} onClick={() => onPageChange(page)}>
+            {page}
+          </button>
+        ))}
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+    );
+  };
 };
 
 export default Pagination;
