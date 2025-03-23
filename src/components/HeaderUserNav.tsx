a2eff7b0c81dd6eb50aad0d14a4212f763ae7305
DQuinn97 <quinten2407@gmail.com>
2025-03-23 21:18:01 +0100

    trying to fix header

--- src/components/HeaderUserNav.tsx
+++ src/components/HeaderUserNav.tsx
@@ -1,23 +1,21 @@
 import { Link } from "react-router";
 import { BsPerson, BsHeart, BsCart3, BsSearch } from "react-icons/bs";
 import styles from "../css_modules/HeaderUserNav.module.css";
+import WeGotBoardLogo from "../images/WeGotBoard_cut.png";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../store/authSlice";
+import { getTotalQuantity } from "../store/cartSlice";
+import SearchBar from "./SearchBar";
 
 const HeaderUserNav = () => {
   const user = useSelector(selectCurrentUser);
-
+  const total = useSelector(getTotalQuantity);
   return (
     <div className={styles.userNav}>
       <Link to="/">
-        <h1>WeGotBoard</h1>
+        <img src={WeGotBoardLogo} alt="logo" />
       </Link>
-      <div className={styles.searchBar}>
-        <input type="text" placeholder="Search products..." />
-        <button>
-          <BsSearch />
-        </button>
-      </div>
+      <SearchBar />
       <ul>
         <li>
           <Link to={user ? "/user/profile" : "/user/login"}>
@@ -38,8 +36,10 @@ const HeaderUserNav = () => {
         <li>
           <Link to="/cart">
             <i>
+              {" "}
               <BsCart3 />
             </i>
+            {total !== 0 && <span className={styles.quantity}>{total}</span>}
             Cart
           </Link>
         </li>
