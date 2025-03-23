4f89c803968e5dd5873c3273b361df273abed4ac
Anouck Galoppin <anouck.galop@hotmail.com>
2025-03-23 09:18:02 +0100

    move dispatches from ProductGrid to ProductListPage for better reusablity of grid

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,60 +1,15 @@
 import styles from "../css_modules/productGrid.module.css";
 import ProductCard from "./ProductCard";
-import { setTotalPages, setProducts } from "../store/paginationSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../store";
-import { useEffect } from "react";
-import { useGetProductsQuery } from "../store/productAPI";
+import { Product } from "../types";
 
-const ProductGrid = () => {
-  const dispatch = useDispatch();
-  const { currentPage, productsPerPage, products } = useSelector(
-    (state: RootState) => state.productGrid
-  );
-  const { selectedSort } = useSelector((state: RootState) => state.sort);
+interface ProductGridProps {
+  products: Product[];
+}
 
-  const startIndex = (currentPage - 1) * productsPerPage;
-  const endIndex = startIndex + productsPerPage;
-
-  const { data: fetchedProducts } = useGetProductsQuery({});
-
-  useEffect(() => {
-    if (fetchedProducts) {
-      dispatch(setProducts(fetchedProducts));
-      dispatch(
-        setTotalPages(Math.ceil(fetchedProducts.length / productsPerPage))
-      );
-    }
-    console.log("Products length:", products.length, productsPerPage);
-  }, [fetchedProducts]);
-
-  useEffect(() => {
-    dispatch(setTotalPages(Math.ceil(products.length / productsPerPage)));
-  }, [productsPerPage]);
-
-  useEffect(() => {
-    if (selectedSort === "ascending") {
-      dispatch(setProducts([...products].sort((a, b) => a.price - b.price)));
-    } else if (selectedSort === "descending") {
-      dispatch(setProducts([...products].sort((a, b) => b.price - a.price)));
-    } else if (selectedSort === "rating") {
-      dispatch(
-        setProducts(
-          [...products].sort((a, b) => {
-            const avgA =
-              a.userRating.reduce((a, b) => a + b, 0) / a.userRating.length;
-            const avgB =
-              b.userRating.reduce((a, b) => a + b, 0) / b.userRating.length;
-            return avgB - avgA;
-          })
-        )
-      );
-    }
-  }, [selectedSort]);
-
+const ProductGrid = ({ products }: ProductGridProps) => {
   return (
     <div className={styles.product_grid}>
-      {products.slice(startIndex, endIndex).map((product) => (
+      {products.map((product) => (
         <ProductCard key={product._id} product={product} />
       ))}
     </div>
--- src/pages/ProductListPage.tsx
+++ src/pages/ProductListPage.tsx
@@ -2,12 +2,62 @@ import { Filter } from "../components/Filter";
 import ProductGrid from "../components/ProductGrid";
 import Pagination from "../components/Pagination";
 import SortBy from "../components/SortBy";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+import { setProducts, setTotalPages } from "../store/paginationSlice";
+import { useGetProductsQuery } from "../store/productAPI";
 
 const ProductListPage = () => {
+  const dispatch = useDispatch();
+  const { currentPage, productsPerPage, products } = useSelector(
+    (state: RootState) => state.productGrid
+  );
+  const { selectedSort } = useSelector((state: RootState) => state.sort);
+
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+
+  const { data: fetchedProducts } = useGetProductsQuery({});
+
+  useEffect(() => {
+    if (fetchedProducts) {
+      dispatch(setProducts(fetchedProducts));
+      dispatch(
+        setTotalPages(Math.ceil(fetchedProducts.length / productsPerPage))
+      );
+    }
+    console.log("Products length:", products.length, productsPerPage);
+  }, [fetchedProducts]);
+
+  useEffect(() => {
+    dispatch(setTotalPages(Math.ceil(products.length / productsPerPage)));
+  }, [productsPerPage]);
+
+  useEffect(() => {
+    if (selectedSort === "ascending") {
+      dispatch(setProducts([...products].sort((a, b) => a.price - b.price)));
+    } else if (selectedSort === "descending") {
+      dispatch(setProducts([...products].sort((a, b) => b.price - a.price)));
+    } else if (selectedSort === "rating") {
+      dispatch(
+        setProducts(
+          [...products].sort((a, b) => {
+            const avgA =
+              a.userRating.reduce((a, b) => a + b, 0) / a.userRating.length;
+            const avgB =
+              b.userRating.reduce((a, b) => a + b, 0) / b.userRating.length;
+            return avgB - avgA;
+          })
+        )
+      );
+    }
+  }, [selectedSort]);
+
   return (
     <div>
       <SortBy />
-      <ProductGrid />
+      <ProductGrid products={products.slice(startIndex, endIndex)} />
       <Filter />
       <Pagination />
     </div>
