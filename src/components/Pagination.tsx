de4e2d80c9506d0a46d68fd4b202f6d1059b3cf3
Anouck Galoppin <anouck.galop@hotmail.com>
2025-03-24 09:08:33 +0100

    add smooth scroll to top when using pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,12 +2,18 @@ import styles from "../css_modules/pagination.module.css";
 import { setCurrentPage } from "../store/paginationSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
+import { useEffect } from "react";
 
 const Pagination = () => {
   const dispatch = useDispatch();
   const { currentPage, totalPages } = useSelector(
     (state: RootState) => state.productGrid
   );
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [currentPage]);
+
   return (
     <div className={styles.gridPagination}>
       <button
