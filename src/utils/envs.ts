71826a0b1851ce2cf05e4d04f36fffdf09d75f97
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-03-24 09:23:57 +0100

    mail template comes through succesfully

--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -1,15 +1,14 @@
 import "dotenv/config";
 
-const { SMTP_HOST, SMTP_USER, SMTP_PASS, TEMPLATE_SENDER_MAIL } = process.env;
+const { SMTP_HOST, SMTP_USER, SMTP_PASS, TEMPLATE_MAILSENDER } = process.env;
 
 console.log("SMTP_HOST:", process.env.SMTP_HOST);
 console.log("SMTP_USER:", process.env.SMTP_USER);
 console.log("SMTP_PASS:", process.env.SMTP_PASS);
-console.log("TEMPLATE_ID_SENDERMAIL:", process.env.TEMPLATE_ID_SENDERMAIL);
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("TEMPLATE_MAILSENDER:", process.env.TEMPLATE_MAILSENDER);
 
 // if (!SMTP_HOST || !SMTP_USER || !SMTP_PASS || !TEMPLATE_SENDER_MAIL) {
 //   throw new Error("SMTP_HOST, SMTP_USER, SMTP_PASS are required");
 // }
 
-export { SMTP_HOST, SMTP_USER, SMTP_PASS, TEMPLATE_SENDER_MAIL };
+export { SMTP_HOST, SMTP_USER, SMTP_PASS, TEMPLATE_MAILSENDER };
--- src/utils/verifyMail.ts
+++ src/utils/verifyMail.ts
@@ -1,15 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { Recipient, EmailParams, MailerSend, Sender } from "mailersend";
-import { TEMPLATE_SENDER_MAIL, SMTP_USER } from "./envs";
+import { TEMPLATE_MAILSENDER, SMTP_USER } from "./envs";
 import { EmailData } from "./types";
 
 export const sendEmail = async (data: EmailData) => {
   try {
     const mailersend = new MailerSend({ apiKey: process.env.MAILERSEND_API_KEY as string });
-
     const recipients = [new Recipient(data.email, data.name)];
-
     const personalization = [
       {
         email: data.email,
@@ -19,22 +17,20 @@ export const sendEmail = async (data: EmailData) => {
         },
       },
     ];
-
     const emailParams = new EmailParams({
       from: new Sender(SMTP_USER as string, "WeGotBoard"),
       to: recipients,
       subject: "Verify your email",
-      templateId: TEMPLATE_SENDER_MAIL,
+      templateId: TEMPLATE_MAILSENDER,
       personalization: personalization,
     });
 
-    if (TEMPLATE_SENDER_MAIL) {
-      emailParams.setTemplateId(TEMPLATE_SENDER_MAIL as string);
+    if (TEMPLATE_MAILSENDER) {
+      emailParams.setTemplateId(TEMPLATE_MAILSENDER as string);
       emailParams.setPersonalization(personalization);
     } else {
       emailParams.setText(`Hello ${data.name}, please verify your email using this link: ${data.link}`);
     }
-
     const response = await mailersend.email.send(emailParams);
     console.log("Email sent successfully:", response);
     return response;
