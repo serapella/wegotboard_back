fdfb3d32593a45b26449c178e801777108cd51d3
DQuinn97 <quinten2407@gmail.com>
2025-03-24 17:05:23 +0100

    send user and token in response

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -70,7 +70,11 @@ export const createUser = async (req: Request, res: Response) => {
 
     res
       .status(201)
-      .json({ message: "User created succesfully", user: response });
+      .json({
+        message: "User created succesfully",
+        token: verificationToken,
+        user: response,
+      });
   } catch (error: unknown) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
@@ -115,7 +119,9 @@ export const loginUser = async (req: Request, res: Response) => {
           maxAge: 60 * 60 * 1000,
         });
 
-        res.status(200).json({ message: "User logged in successfully", token });
+        res
+          .status(200)
+          .json({ message: "User logged in successfully", token, user });
       } else {
         res.status(401).json({
           message: "Password is incorrect",
