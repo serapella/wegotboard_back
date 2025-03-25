d41a8aba4c227b79d1dbe56d7161f4ca36988be1
Serap Cakmak <serap.cakmak@hotmail.com>
2025-03-25 03:18:02 +0100

    errrorhandling login en register

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,5 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import wishlistReducer from "./wishlistSlice";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import productAPI from "./productAPI";
@@ -11,8 +10,6 @@ import filterReducer from "./filterSlice";
 import counterReducer from "./counterSlice";
 import authReducer from "./authSlice";
 import userAPI from "./userAPI";
-import productGridReducer from "./paginationSlice";
-import sortReducer from "./sortSlice";
 
 const persistConfig = {
   key: "wgb_root",
@@ -22,12 +19,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  wishlistSlice: wishlistReducer,
   counterSlice: counterReducer,
   cartSlice: cartReducer,
   filter: filterReducer,
-  productGrid: productGridReducer,
-  sort: sortReducer,
   [productAPI.reducerPath]: productAPI.reducer,
   [newsAPI.reducerPath]: newsAPI.reducer,
   [reviewAPI.reducerPath]: reviewAPI.reducer,
@@ -43,10 +37,17 @@ const store = configureStore({
       serializableCheck: {
         ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
       },
-    }).concat(logger, productAPI.middleware, newsAPI.middleware, reviewAPI.middleware, userAPI.middleware),
+    }).concat(
+      logger,
+      productAPI.middleware,
+      newsAPI.middleware,
+      reviewAPI.middleware,
+      userAPI.middleware
+    ),
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 export const persistor = persistStore(store);
 export default store;
