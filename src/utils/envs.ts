628d407e36137b8daced7fda25d3471807339102
Kevin Van Damme <kevin.van.damme.pxl@protonmail.com>
2025-03-25 05:29:08 +0100

    changes to env

--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -2,13 +2,13 @@ import "dotenv/config";
 
 const { SMTP_HOST, SMTP_USER, SMTP_PASS, TEMPLATE_MAILSENDER, BASE_URL, MAILERSEND_API_KEY } = process.env;
 
-console.log("SMTP_HOST:", process.env.SMTP_HOST);
-console.log("SMTP_USER:", process.env.SMTP_USER);
-console.log("SMTP_PASS:", process.env.SMTP_PASS);
-console.log("TEMPLATE_MAILSENDER:", process.env.TEMPLATE_MAILSENDER);
+// console.log("SMTP_HOST:", process.env.SMTP_HOST);
+// console.log("SMTP_USER:", process.env.SMTP_USER);
+// console.log("SMTP_PASS:", process.env.SMTP_PASS);
+// console.log("TEMPLATE_MAILSENDER:", process.env.TEMPLATE_MAILSENDER);
 
-// if (!SMTP_HOST || !SMTP_USER || !SMTP_PASS || !TEMPLATE_SENDER_MAIL) {
-//   throw new Error("SMTP_HOST, SMTP_USER, SMTP_PASS are required");
-// }
+if (!SMTP_HOST || !SMTP_USER || !SMTP_PASS || !TEMPLATE_MAILSENDER) {
+  throw new Error("SMTP_HOST, SMTP_USER, SMTP_PASS are required");
+}
 
 export { SMTP_HOST, SMTP_USER, SMTP_PASS, TEMPLATE_MAILSENDER, BASE_URL, MAILERSEND_API_KEY };
